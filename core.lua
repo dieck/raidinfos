5a6f1fcfdf4ed6ba5c98749aab4a84f02a77f402
Marco Dieckhoff <marco.dieckhoff@googlemail.com>
2020-06-30 18:45:04 +0200

    fixes #1 now removes all expired raids; fixes #2 now displays realm; fixes display error on reset dates

--- core.lua
+++ core.lua
@@ -5,14 +5,15 @@ function Raidinfos:OnInitialize()
   -- Code that you want to run when the addon is first loaded goes here.
 
   self.db = LibStub("AceDB-3.0"):New("RaidinfosDB", defaults)
-  if self.db.global.instances == nil then self.db.global.instances = {} end
+  
+  self.db.global.instances = nil -- old version data
+  if self.db.global.savedinstances == nil then self.db.global.savedinstances = {} end
 	
   self:RegisterChatCommand("raidinfos", "ShowRaidinfos")
   self.RaidinfosFrameVar = nil
   
   -- main event: do something when the Loot Window is being shown
   Raidinfos:RegisterEvent("PLAYER_ENTERING_WORLD")
---  Raidinfos:RegisterEvent("START_LOOT_ROLL")
   
 end
 
@@ -31,20 +32,34 @@ end
 
 
 function Raidinfos:RemoveOldRaids()
-	if Raidinfos.db.global.instances[GetUnitName("player")] == nil then return end
+	if Raidinfos.db.global.savedinstances[GetUnitName("player")] == nil then return end
 	
-	for i,r in pairs(Raidinfos.db.global.instances[GetUnitName("player")]) do
-		if r["r"] <= time() then
-			Raidinfos.db.global.instances[GetUnitName("player")][r["i"]] = nil
-		end		
+	for realm,players in pairs(Raidinfos.db.global.savedinstances) do
+		for player,raids in pairs(players) do
+			for i,r in pairs(raids) do
+				if r["r"] <= time() then
+					Raidinfos.db.global.savedinstances[realm][player][r["i"]] = nil
+				end		
+			end
+		end
 	end
 end
+
+
+GetRealmName()
 
 function Raidinfos:SaveRaids()
 	-- always also expire raids when saving new
 	Raidinfos:RemoveOldRaids()
 	
-	if Raidinfos.db.global.instances[GetUnitName("player")] == nil then Raidinfos.db.global.instances[GetUnitName("player")] = {} end
+	player = GetUnitName("player")
+	realm = GetRealmName()
+	
+	-- longest (current, Classic) realm for debug purposes
+	-- realm = "Hydraxian Waterlords"
+	
+	if Raidinfos.db.global.savedinstances[realm] == nil then Raidinfos.db.global.savedinstances[realm] = {} end
+	if Raidinfos.db.global.savedinstances[realm][player] == nil then Raidinfos.db.global.savedinstances[realm][player] = {} end
 	
 	numInstances = GetNumSavedInstances()
 
@@ -52,24 +67,28 @@ function Raidinfos:SaveRaids()
 		name, id, seconds = GetSavedInstanceInfo(i)
 		
 		reset = time() + seconds
-		resetstring = date("%A, %m %B %Y %H:%M", reset)
+		resetstring = date("%d %B %Y %H:%M (%A)", reset)
+		
+		-- longest month for debug purposes
+		-- resetstring = "31 September 2020 20:20 (Wednesday)"
 
-		Raidinfos.db.global.instances[GetUnitName("player")][id] = {n = name, i = id, r = reset, s = resetstring}
-		 
+		Raidinfos.db.global.savedinstances[realm][player][id] = {srv = realm, n = name, i = id, r = reset, s = resetstring}
 	end
 	
 end
 
 function Raidinfos:ShowRaidinfos()
 	Raidinfos.RaidinfosFrameVar = Raidinfos:createRaidinfosFrame()
-	Raidinfos.RaidinfosFrameVar:Show()
+	if Raidinfos.RaidinfosFrameVar then 
+		Raidinfos.RaidinfosFrameVar:Show()
+	end
 end
 
 function Raidinfos:createRaidinfosFrame()
 	local AceGUI = LibStub("AceGUI-3.0")
 
 	Raidinfos:SaveRaids()
-	if Raidinfos.db.global.instances[GetUnitName("player")] == nil then 
+	if Raidinfos.db.global.savedinstances == nil then 
 		Raidinfos:Print("No Lockouts.")
 		return;
 	end
@@ -78,7 +97,7 @@ function Raidinfos:createRaidinfosFrame()
 	f:SetTitle("Raidinfos")
 	f:SetStatusText("")
 	f:SetLayout("Flow")
-	f:SetWidth(500)
+	f:SetWidth(640)
 	f:SetHeight(200)
 	f:SetCallback("OnClose",function(widget) AceGUI:Release(widget) end)
 	
@@ -97,53 +116,69 @@ function Raidinfos:createRaidinfosFrame()
 	s:SetLayout("Flow") -- probably?
 	scrollcontainer:AddChild(s)
 
+	
+	sizes = {0.19, 0.18, 0.17, 0.12, 0.34}
+	
+	local lbHR = AceGUI:Create("Label")
+	lbHR:SetText("Realm")
+	lbHR:SetColor(0,0.6,1)
+	lbHR:SetRelativeWidth(sizes[1])
+	s:AddChild(lbHR)
+
 	local lbHR = AceGUI:Create("Label")
 	lbHR:SetText("Player")
 	lbHR:SetColor(0,0.6,1)
-	lbHR:SetRelativeWidth(0.18)
+	lbHR:SetRelativeWidth(sizes[2])
 	s:AddChild(lbHR)
 
 	local lbHR = AceGUI:Create("Label")
 	lbHR:SetText("Raid")
 	lbHR:SetColor(0,0.6,1)
-	lbHR:SetRelativeWidth(0.20)
+	lbHR:SetRelativeWidth(sizes[3])
 	s:AddChild(lbHR)
 
 	local lbHR = AceGUI:Create("Label")
 	lbHR:SetText("ID")
 	lbHR:SetColor(0,0.6,1)
-	lbHR:SetRelativeWidth(0.15)
+	lbHR:SetRelativeWidth(sizes[4])
 	s:AddChild(lbHR)
 
 	local lbHR = AceGUI:Create("Label")
 	lbHR:SetText("Expire")
 	lbHR:SetColor(0,0.6,1)
-	lbHR:SetRelativeWidth(0.47)
+	lbHR:SetRelativeWidth(sizes[5])
 	s:AddChild(lbHR)
 	
-	for player,raids in pairs(Raidinfos.db.global.instances) do
-		for id,raid in pairs(Raidinfos.db.global.instances[player]) do
+	for realm,players in pairs(Raidinfos.db.global.savedinstances) do
+		for player,raids in pairs(players) do
+			for i,raid in pairs(raids) do
 
-			local lbPlayerName = AceGUI:Create("Label")
-			lbPlayerName:SetText(player)
-			lbPlayerName:SetRelativeWidth(0.18)
-			s:AddChild(lbPlayerName)
+				local lbPlayerName = AceGUI:Create("Label")
+				lbPlayerName:SetText(realm)
+				lbPlayerName:SetRelativeWidth(sizes[1])
+				s:AddChild(lbPlayerName)
 
-			local lbRaid = AceGUI:Create("Label")
-			lbRaid:SetText(raid["n"])
-			lbRaid:SetRelativeWidth(0.20)
-			s:AddChild(lbRaid)
+				local lbPlayerName = AceGUI:Create("Label")
+				lbPlayerName:SetText(player)
+				lbPlayerName:SetRelativeWidth(sizes[2])
+				s:AddChild(lbPlayerName)
 
-			local lbID = AceGUI:Create("Label")
-			lbID:SetText(raid["i"])
-			lbID:SetRelativeWidth(0.15)
-			s:AddChild(lbID)
+				local lbRaid = AceGUI:Create("Label")
+				lbRaid:SetText(raid["n"])
+				lbRaid:SetRelativeWidth(sizes[3])
+				s:AddChild(lbRaid)
 
-			local lbReset = AceGUI:Create("Label")
-			lbReset:SetText(raid["s"])
-			lbReset:SetRelativeWidth(0.47)
-			s:AddChild(lbReset)
-			
+				local lbID = AceGUI:Create("Label")
+				lbID:SetText(raid["i"])
+				lbID:SetRelativeWidth(sizes[4])
+				s:AddChild(lbID)
+
+				local lbReset = AceGUI:Create("Label")
+				lbReset:SetText(raid["s"])
+				lbReset:SetRelativeWidth(sizes[5])
+				s:AddChild(lbReset)
+	
+			end
 		end
 	end
 	
